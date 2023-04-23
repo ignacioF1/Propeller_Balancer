9ba8fad3beb9a5bfb0525847b23aebbbaecb9bb0
ignacioF1 <ignacioamendizabal@gmail.com>
2023-04-23 13:23:05 +0200

    Code update

--- Prop_balancer_LCD16x2.006.ino
+++ Prop_balancer_LCD16x2.006.ino
@@ -9,16 +9,16 @@
 #define SAMPLES_ANGLE_AVG 600     //Samples taken to calculate angle average
 #define LCD_UPDATE_COUNT 200      //Main loop pases till the LCD is updated
 //
-//Inicializar un objeto de la librería
+//Initialize library object
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified();
 
-// CONEXIONES
-// LCD 1602A (5V, GND, SCL(A5), SDA(A4))
-// IR sensor HW-201 (5V, GND, pin 8)
-// ADXL345 (3.3V, GND, SCL(A5), SDA(A4))
-// ADXL345 position -> align sensor's x & y coordinates to one corner, and the IR sensor should be where y axis is pointing, in the middle of the fan. 
+//CONNECTIONS
+//LCD 1602A (5V, GND, SCL(A5), SDA(A4))
+//IR sensor HW-201 (5V, GND, pin 8)
+//ADXL345 (3.3V, GND, SCL(A5), SDA(A4))
+//ADXL345 position -> align sensor's x & y coordinates to one corner, and the IR sensor should be where y axis is pointing, in the middle of the fan. 
 
-//Global variables
+//GLOBAL VARIABLES
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 // Make custom character:
 byte Check[8] =
@@ -65,7 +65,7 @@ void setup() {
   // 16MHz / 64 => 1bit 0,000004s (4us)
   //TCCR1B b7=1(noise cancel) b6=0(falling edge) b2-0(prescaler)
   //TIMSK1 b5=1(enable input capture)
-  cli();                //disable interrupts
+  cli();                //Disable interrupts
   TCCR1A = 0;           //Initialize timer1 mode
   TCCR1B = 0;           
   TCCR1B |= 0b10000011; //Set noise canceler, falling edge, 64 prescaler
@@ -86,9 +86,9 @@ void setup() {
   //
 
   //ADXL345 initialization
-  //Verificar si hay comunicación entre el sensor y la placa
+  //Verify connection between sensor and controller
   if(!accel.begin()) {
-    Serial.println("El sensor no inició");
+    Serial.println("The accel sensor didn't start!");
     while(1);
   }
   /* Set the data rate */
@@ -112,27 +112,27 @@ void loop() {
   sensors_event_t event;
   accel.getEvent(&event);
   actualAccel = event.acceleration.x - xc;
-  //Serial.println("X: " + x + " m/s^2);
+  //Serial.println(actualAccel);
   //
-    actualPeriod = TCNT1;
-    if (actualAccel > maxAccel) {
-      maxAccel += 0.01;
-      actualAngle = ((float)actualPeriod/(float)lastPeriod)*360;
-      updateAngle();
-    } else if (actualAccel > 0 && actualAccel < maxAccel) {
-      maxAccel -= 0.01;
-    } else if (actualAccel < 0 && abs(actualAccel) > maxAccel) {
-      maxAccel += 0.01;
-      actualAngle = ((float)actualPeriod/(float)lastPeriod)*360;
-      if (actualAngle >= 180) {
-        actualAngle -= 180;  
-      } else {
-        actualAngle += 180;
-      }
-      updateAngle();
-    }  else if (actualAccel < 0 && abs(actualAccel) < maxAccel) {
-      maxAccel -= 0.01;
+  actualPeriod = TCNT1;
+  if (actualAccel > maxAccel) {
+    maxAccel += 0.01;
+    actualAngle = ((float)actualPeriod/(float)lastPeriod)*360;
+    updateAngle();
+  } else if (actualAccel > 0 && actualAccel < maxAccel) {
+    maxAccel -= 0.01;
+  } else if (actualAccel < 0 && abs(actualAccel) > maxAccel) {
+    maxAccel += 0.01;
+    actualAngle = ((float)actualPeriod/(float)lastPeriod)*360;
+    if (actualAngle >= 180) {
+      actualAngle -= 180;  
+    } else {
+      actualAngle += 180;
     }
+    updateAngle();
+  } else if (actualAccel < 0 && abs(actualAccel) < maxAccel) {
+    maxAccel -= 0.01;
+  }
   //Serial.println(lastPeriod);
   
   //Update LCD after some time
@@ -145,47 +145,47 @@ void loop() {
 }
 
 void updateAngle() {
-    if (actualAngle >= 0 && actualAngle <= 360) {
-        angProm++;
-        if (angProm == SAMPLES_ANGLE_AVG){
-          s_numerator = promAccelAngle_y / SAMPLES_ANGLE_AVG;
-          c_denominator = promAccelAngle_x / SAMPLES_ANGLE_AVG;
-        if (s_numerator > 0) {
-          maxAccelAngle = atan2 (s_numerator , c_denominator) * 180 / M_PI;
-        }else{
-            maxAccelAngle = (atan2 (s_numerator , c_denominator) * 180 / M_PI) + 360;
-        }
-          angProm = 0;
-          promAccelAngle_x = 0;
-          promAccelAngle_y = 0;
-          }
-          promAccelAngle_x += cos (actualAngle * M_PI / 180);
-          promAccelAngle_y += sin (actualAngle * M_PI / 180);
+  if (actualAngle >= 0 && actualAngle <= 360) {
+    angProm++;
+    if (angProm == SAMPLES_ANGLE_AVG){
+      s_numerator = promAccelAngle_y / SAMPLES_ANGLE_AVG;
+      c_denominator = promAccelAngle_x / SAMPLES_ANGLE_AVG;
+      if (s_numerator > 0) {
+        maxAccelAngle = atan2 (s_numerator , c_denominator) * 180 / M_PI;
+      }else{
+        maxAccelAngle = (atan2 (s_numerator , c_denominator) * 180 / M_PI) + 360;
+      }
+      angProm = 0;
+      promAccelAngle_x = 0;
+      promAccelAngle_y = 0;
     }
+    promAccelAngle_x += cos (actualAngle * M_PI / 180);
+    promAccelAngle_y += sin (actualAngle * M_PI / 180);
+  }
 }
       
 void updateLCD() {
-      // Update LCD
-      lcd.clear();
-      // Line 1
-      lcd.setCursor(0,0);
-      lcd.print("Max accel:");
-      lcd.setCursor(11,0);
-      lcd.print(maxAccel);
-      if(int_updated && maxAccel <= BALANCED_ACCEL) {
-        lcd.setCursor(15,0);
-        lcd.write(byte(0));
-      }
-      // Line 2
-      if (int_updated){
-        lcd.setCursor(0,1);
-        lcd.print("Heavy at:");
-        lcd.setCursor(10,1);
-        lcd.print(maxAccelAngle);
-        lcd.setCursor(15,1);
-        lcd.print(char(223));
-      }else{
-        lcd.setCursor(0,1);
-        lcd.print("NO RPM!");
-      }
+  //Update LCD
+  lcd.clear();
+  // Line 1
+  lcd.setCursor(0,0);
+  lcd.print("Max accel:");
+  lcd.setCursor(11,0);
+  lcd.print(maxAccel);
+  if(int_updated && maxAccel <= BALANCED_ACCEL) {
+    lcd.setCursor(15,0);
+    lcd.write(byte(0));
   }
+  // Line 2
+  if (int_updated){
+    lcd.setCursor(0,1);
+    lcd.print("Heavy at:");
+    lcd.setCursor(10,1);
+    lcd.print(maxAccelAngle);
+    lcd.setCursor(15,1);
+    lcd.print(char(223));
+  }else{
+    lcd.setCursor(0,1);
+    lcd.print("NO RPM!");
+  }
+}
